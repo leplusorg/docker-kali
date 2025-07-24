d4b39c8d1892ca734cd2d7cbeeb4bd2ee52d4242
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-07-24 21:36:47 +0200

    Add Docker environment support to bump-apt-versions.sh
    Script now detects if running inside a Docker container and executes apt version bumping locally. If not in Docker, it runs itself inside a Docker container for consistent environment handling.

--- kali/bump-apt-versions.sh
+++ kali/bump-apt-versions.sh
@@ -5,14 +5,22 @@ IFS=$'\n\t'
 
 cd "$(\dirname "$0")"
 
-\apt-get update
+if [ -f /.dockerenv ]; then
 
-\sed -e :a -e '/\\$/N; s/\\\n//; ta' Dockerfile | \grep -o -e 'apt-get[^\&\;]*install[^\&\;]*' | \grep -o -e '[^ ]*\=[^ ]*' | while IFS= read -r l; do
-	p="${l%=*}"
-	v1="${l#*=}"
-	v2="$(\apt-cache policy "${p}" | \grep -o -e 'Candidate: [^ ]*' | \sed -e 's/^Candidate: //')"
-	if [ "${v1}" != "${v2}" ]; then
-		\echo "${p} ${v1} -> ${v2}"
-		\perl -i -p -e "s|\Q${l}\E|${p}=${v2}|g" Dockerfile
-	fi
-done
+	\apt-get update
+
+	\sed -e :a -e '/\\$/N; s/\\\n//; ta' Dockerfile | \grep -o -e 'apt-get[^\&\;]*install[^\&\;]*' | \grep -o -e '[^ ]*\=[^ ]*' | while IFS= read -r l; do
+		p="${l%=*}"
+		v1="${l#*=}"
+		v2="$(\apt-cache policy "${p}" | \grep -o -e 'Candidate: [^ ]*' | \sed -e 's/^Candidate: //')"
+		if [ "${v1}" != "${v2}" ]; then
+			\echo "${p} ${v1} -> ${v2}"
+			\perl -i -p -e "s|\Q${l}\E|${p}=${v2}|g" Dockerfile
+		fi
+	done
+
+else
+
+	\docker run --pull always --rm -t --user root -v "$(pwd):/opt/bump" "leplusorg/${PWD##*/}:main" /opt/bump/"$(\basename "${0}")"
+
+fi
