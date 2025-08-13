a180c715060b6428b1334d22a025b7c196428bca
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-08-13 16:17:24 +0200

    style: remove unnecessary escape for builtin

--- kali/bump-apt-versions.sh
+++ kali/bump-apt-versions.sh
@@ -14,8 +14,8 @@ if [ -f /.dockerenv ]; then
 		v1="${l#*=}"
 		v2="$(\apt-cache policy "${p}" | \grep -o -e 'Candidate: [^ ]*' | \sed -e 's/^Candidate: //')"
 		if [ "${v1}" != "${v2}" ]; then
-			\echo "${p} ${v1} -> ${v2}"
-			\echo "${p},${v1},${v2},${p}=${v1},${p}=${v2}" >>.bump.csv.bak
+			echo "${p} ${v1} -> ${v2}"
+			echo "${p},${v1},${v2},${p}=${v1},${p}=${v2}" >>.bump.csv.bak
 		fi
 	done
 
